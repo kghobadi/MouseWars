a2bb71ce607ac48caae11c390a75280791d36e6f
Easton <eastonself@gmail.com>
2021-09-13 15:55:24 -0400

    updated card gaze location

--- Assets/Scripts/CreatureBehavior.cs
+++ Assets/Scripts/CreatureBehavior.cs
@@ -244,7 +244,7 @@ public class CreatureBehavior : AudioHandler
             //get midpoint between the 2 objects
             Vector3 midpoint = (transform.position + enemyCreature.transform.position) / 2;
             //spawn tussle cloud 
-            tussleCloudClone = Instantiate(GameManager.Instance.tussleCloudPrefab, midpoint, Quaternion.identity);
+            tussleCloudClone = Instantiate(GameManager.Instance.tussleCloudPrefab, midpoint + new Vector3(0, 0.7f, 0), Quaternion.identity);
             
             //see who has bigger attack value
             if (myCardData.damage > enemyCreature.myCardData.damage)
