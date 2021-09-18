a54d1f1a6a3fb4fa417ff38a0fcda449331d3246
kghobadi <kasghobadi@gmail.com>
2021-09-18 10:11:09 -0700

    fixed up some stuff in the scene, got mouse cameras working, made a prefab for main cam.

--- Assets/Scripts/Creatures/CreatureBehavior.cs
+++ Assets/Scripts/Creatures/CreatureBehavior.cs
@@ -30,7 +30,7 @@ public class CreatureBehavior : AudioHandler
 
     private NavMeshAgent creatureNavMeshAgent;
     private CreatureAnimation _creatureAnimation;
-    private GameCamera mouseCamera;
+    public GameCamera mouseCamera;
     public int creatureHP;
 
     public CreatureStates creatureState;
@@ -99,17 +99,13 @@ public class CreatureBehavior : AudioHandler
         gameManager = GameManager.Instance;
         cameraManager = gameManager.GetCameraManager();
         creatureNavMeshAgent = GetComponent<NavMeshAgent>();
-        mouseCamera = GetComponentInChildren<GameCamera>();
         movementFlag = GetComponentInChildren<MovementFlag>();
         movementFlag.DeactivateFlag();
-        
         
         //add event listeners
         GameManager.Instance.changedPhases.AddListener(OnChangedPhases);
         GameManager.Instance.changedPlayers.AddListener(OnChangedPlayer);
-
         
-
         init = true;
     }
     
@@ -230,7 +226,7 @@ public class CreatureBehavior : AudioHandler
         teamHand.SetCanHold(true);
         playerIsMovingMe = false;
         //return to prev camera
-        gameManager.GetCameraManager().ReturnToPrevCamera();
+        cameraManager.ReturnToPrevCamera();
     }
 
     public virtual void SetMoveLocation()
@@ -262,6 +258,7 @@ public class CreatureBehavior : AudioHandler
         creatureHasMove = false;
     }
 
+    //called when creature safely reaches its move destination
     public void SetIdle()
     {
         creatureNavMeshAgent.isStopped = true;
